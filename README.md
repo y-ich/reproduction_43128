684f9696a8046e00e0e1be515659aaebfb27cdd1
ICHIKAWA, Yuji <ichikawa.yuji@gmail.com>
2020-09-11 23:39:40 +0900

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# Reproduction code for 43128
+
+I provide the converted model which runs slowly on Kirin 990 and conversion script.
+The model is tf1/model.tflite, and the script is tf1/convert_tflite.py.
+
+Please check the difference between the model and mobilenet_v1_1.0_224_quant.tflite, which run fast on Kirin 990.
---
+++ tf1/convert_tflite.py
@@ -0,0 +1,29 @@
+import sys
+import numpy as np
+import tensorflow as tf
+from board import Board, PASS
+
+if len(sys.argv) < 3:
+    print(f"Usage {sys.argv[0]} <pb file> <tflite file> [float16/int8]")
+    sys.exit()
+
+converter = tf.lite.TFLiteConverter.from_saved_model(sys.argv[1])
+
+if len(sys.argv) == 4:
+    if sys.argv[3] == "float16":
+        converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_LATENCY]
+        converter.target_spec.supported_types = [tf.float16]
+    elif sys.argv[3] == "int8":
+        #converter.experimental_enable_mlir_converter = True
+        converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_LATENCY]
+        def representative_dataset_gen():
+            b = Board()
+            yield [np.array(b.feature().reshape([1, 19, 19, 18]), dtype=np.float32)]
+            while b.random_play() != PASS:
+                yield [np.array(b.feature().reshape([1, 19, 19, 18]), dtype=np.float32)]
+
+        converter.representative_dataset = representative_dataset_gen
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+
+tflite_model = converter.convert()
+open(sys.argv[2], "wb").write(tflite_model)
