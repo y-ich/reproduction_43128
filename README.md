06d0f58b2725789750d76049bf36b5569093bbb0
ICHIKAWA, Yuji <ichikawa.yuji@gmail.com>
2020-09-16 08:44:49 +0900

    model.tflite was not quantized

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Reproduction code for 43128
 
 I provide the converted model which runs slowly on Kirin 990 and conversion script.
-The model is tf1/model.tflite, and the script is tf1/convert_tflite.py.
+tf1/convert_tflite.py is a conversion script.
+tf1/int8.tflite is a model which weights were converted as int8.
+tf1/full_int8.tflite is a model which both input/output and weights were converted as int8.
 
 Please check the difference between the model and mobilenet_v1_1.0_224_quant.tflite, which run fast on Kirin 990.
--- tf1/convert_tflite.py
+++ tf1/convert_tflite.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from board import Board, PASS
 
 if len(sys.argv) < 3:
-    print(f"Usage {sys.argv[0]} <pb file> <tflite file> [float16/int8]")
+    print(f"Usage {sys.argv[0]} <saved model dir> <tflite file> [float16/int8/full_int8]")
     sys.exit()
 
 converter = tf.lite.TFLiteConverter.from_saved_model(sys.argv[1])
@@ -13,8 +13,8 @@ if len(sys.argv) == 4:
     if sys.argv[3] == "float16":
         converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_LATENCY]
         converter.target_spec.supported_types = [tf.float16]
-    elif sys.argv[3] == "int8":
-        #converter.experimental_enable_mlir_converter = True
+    elif sys.argv[3] == "int8" or sys.argv[3] == "full_int8":
+        converter.experimental_enable_mlir_converter = True
         converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_LATENCY]
         def representative_dataset_gen():
             b = Board()
@@ -24,6 +24,9 @@ if len(sys.argv) == 4:
 
         converter.representative_dataset = representative_dataset_gen
         converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+        if sys.argv[3] == "full_int8":
+            converter.inference_input_type = tf.int8
+            converter.inference_output_type = tf.int8
 
 tflite_model = converter.convert()
 open(sys.argv[2], "wb").write(tflite_model)
